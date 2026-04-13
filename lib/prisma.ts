cc83fd7e235a53ede6299da736266d29660cab9b
Halil Sert <Halil_sert2002@hotmail.com>
2026-04-13 20:56:13 +0300

    Remove unnecessary ts-expect-error directive
    Assisted-By: docker-agent

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,12 +4,10 @@ const globalForPrisma = global as typeof global & {
   prisma?: PrismaClient
 }
 
-export const prisma: PrismaClient = globalForPrisma.prisma ?? (() => {
-  // @ts-expect-error - Prisma generates this at build time
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { PrismaClient } = require('@prisma/client/default')
-  return new PrismaClient()
-})()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PrismaClient } = require('@prisma/client/default')
+
+export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
